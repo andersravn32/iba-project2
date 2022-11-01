0c3b996533bc36496d6eeb4b7ac9c9bddbc7ba16
Anders Ravn <anders@adriansen.dk>
2022-11-01 11:43:49 +0100

    Add: Added weather requests

--- app.js
+++ app.js
@@ -1,3 +1,6 @@
+// Global API key
+const API_KEY = "";
+
 // Shorthand query selector
 const $ = (element) => {
     if (document.querySelectorAll(element).length > 1){
@@ -6,3 +9,20 @@ const $ = (element) => {
     return document.querySelector(element);
 }
 
+// Get geocoding for a specific location
+const getLocation = async (input, limit = 5) => {
+    await fetch(`http://api.openweathermap.org/geo/1.0/direct?q=${input}&limit=${limit}&appid=${API_KEY}`)
+    .then(res => res.json())
+    .then((data) => {
+        console.log(data)
+    })
+}
+
+// Get weather data for a specific location
+const getWeatherData = async ({lat, lon}) => {
+    await fetch(`https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=${API_KEY}`)
+    .then(res => res.json())
+    .then((data) => {
+        console.log(data)
+    })
+}
