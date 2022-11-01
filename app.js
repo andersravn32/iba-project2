1e8f144c2672a024d09d00ae2074124d0d4cb7ed
Anders Ravn <anders@adriansen.dk>
2022-11-01 12:29:22 +0100

    Add: Added data fetching methods

--- app.js
+++ app.js
@@ -1,28 +1,45 @@
 // Global API key
-const API_KEY = "";
+const API_KEY = "49b4cd9bd298bc73a9a91553c7db7213";
 
 // Shorthand query selector
 const $ = (element) => {
-    if (document.querySelectorAll(element).length > 1){
-        return document.querySelectorAll(element);
-    }
-    return document.querySelector(element);
-}
+  if (document.querySelectorAll(element).length > 1) {
+    return document.querySelectorAll(element);
+  }
+  return document.querySelector(element);
+};
 
 // Get geocoding for a specific location
-const getLocation = async (input, limit = 5) => {
-    await fetch(`http://api.openweathermap.org/geo/1.0/direct?q=${input}&limit=${limit}&appid=${API_KEY}`)
-    .then(res => res.json())
+const getLocation = async (input, limit = 1) => {
+  const request = await fetch(
+    `http://api.openweathermap.org/geo/1.0/direct?q=${input}&limit=${limit}&appid=${API_KEY}`
+  )
+    .then((res) => res.json())
     .then((data) => {
-        console.log(data)
-    })
-}
+      return data;
+    });
+  return request[0];
+};
 
 // Get weather data for a specific location
-const getWeatherData = async ({lat, lon}) => {
-    await fetch(`https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=${API_KEY}`)
-    .then(res => res.json())
+const getWeatherData = async ({ lat, lon }) => {
+  const request = await fetch(
+    `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=${API_KEY}`
+  )
+    .then((res) => res.json())
     .then((data) => {
-        console.log(data)
-    })
-}
+      return data;
+    });
+  return request;
+};
+
+// Search in for correct geocoding and fetch new weather data
+const search = async (query) => {
+
+  // Used for searching for location data
+  const locationQuery = await getLocation(query);
+
+  // Used for searching for weather data
+  const weatherQuery = await getWeatherData({ lat: locationQuery.lat, lon: locationQuery.lon });
+    console.log(weatherQuery)
+};
