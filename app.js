ceaec90de5905c599f47ce47ca8449cbe96ae8e7
Anders Ravn <anders@adriansen.dk>
2022-11-03 00:40:32 +0100

    Add: Added update method logic

--- app.js
+++ app.js
@@ -4,27 +4,12 @@ const API_KEY = "49b4cd9bd298bc73a9a91553c7db7213";
 // Get searchHistory if any was found
 const searchHistory = JSON.parse(localStorage.getItem("searchHistory")) || [];
 
-// weatherData placeholder, stores data about currently loaded weather
-const weatherData = {};
-
 // Shorthand query selector
 const $ = (element) => {
   if (document.querySelectorAll(element).length > 1) {
     return document.querySelectorAll(element);
   }
   return document.querySelector(element);
-};
-
-// Get geocoding for a specific location
-const getLocation = async (input, limit = 1) => {
-  const request = await fetch(
-    `http://api.openweathermap.org/geo/1.0/direct?q=${input}&limit=${limit}&appid=${API_KEY}`
-  )
-    .then((res) => res.json())
-    .then((data) => {
-      return data;
-    });
-  return request[0];
 };
 
 // Get weather data for a specific location
@@ -39,22 +24,91 @@ const getWeatherData = async ({ lat, lon }) => {
   return request;
 };
 
-// Search in for correct geocoding and fetch new weather data
-const search = async (query) => {
-  // Used for searching for location data
-  const locationQuery = await getLocation(query);
+const update = async ({ lat, lon }) => {
+  // Request weather data regarding location
+  const request = await fetch(
+    `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=${API_KEY}`
+  ).then((res) => res.json());
 
-  // Push to searchHistory and update data
-  searchHistory.push(locationQuery);
+  console.log(request);
+  // Update main view location title
+  $("#app-weather-location-data")
+    .querySelector("h1")
+    .querySelector("span").innerText = request.name;
 
-  // Update localStorage with new searchHistory
-  localStorage.setItem("searchHistory", JSON.stringify(searchHistory));
-  console.log(locationQuery);
-  // Update the UI with the updated location
-  update({
-    lat: locationQuery.lat,
-    lon: locationQuery.lon,
-  });
+  // Update date
+  $("#app-weather-location-data").querySelectorAll("p")[0].innerText =
+    new Intl.DateTimeFormat("da-DK", { dateStyle: "full", timeStyle: "short" })
+      .format(new Date(Date.now() + 1000 * request.timezone))
+      .split("kl.")[0];
+
+  // Update time
+  $("#app-weather-location-data").querySelectorAll("p")[1].innerText =
+    new Intl.DateTimeFormat("da-DK", { dateStyle: "full", timeStyle: "short" })
+      .format(new Date(Date.now() + 1000 * request.timezone))
+      .split("kl.")[1].replace(".", ":");
+
+  // Update celcius temperature
+  $("#app-weather-data-temperature").querySelector(
+    "h2"
+  ).innerText = `${Math.floor(request.main.temp - 273.15)}°C`;
+
+  // Update arrow rotation and wind speed
+  $("#app-weather-data-wind").querySelector(
+    "svg"
+  ).style.transform = `rotate(${request.wind.deg}deg)`;
+  $("#app-weather-data-wind").querySelector(
+    "p"
+  ).innerText = `${request.wind.speed} m/s`;
+
+  // Update app background
+  if (request.weather[0].id >= 200 && request.weather[0].id <= 232) {
+    $("#app-weather-data-temperature")
+      .querySelector("p")
+      .querySelector("span").innerText = "Tordenvejr";
+    $("#app").removeAttribute("class");
+    $("#app").classList.toggle("bg-rain");
+  }
+
+  if (request.weather[0].id >= 300 && request.weather[0].id <= 321) {
+    $("#app-weather-data-temperature")
+      .querySelector("p")
+      .querySelector("span").innerText = "Let regnvejr";
+    $("#app").removeAttribute("class");
+    $("#app").classList.toggle("bg-rain");
+  }
+
+  if (request.weather[0].id >= 500 && request.weather[0].id <= 531) {
+    $("#app-weather-data-temperature")
+      .querySelector("p")
+      .querySelector("span").innerText = "Regnvejr";
+    $("#app").removeAttribute("class");
+    $("#app").classList.toggle("bg-rain");
+  }
+
+  if (request.weather[0].id >= 600 && request.weather[0].id <= 622) {
+    $("#app-weather-data-temperature")
+      .querySelector("p")
+      .querySelector("span").innerText = "Snevejr";
+    $("#app").removeAttribute("class");
+    $("#app").classList.toggle("bg-snow");
+  }
+
+  if (request.weather[0].id >= 701 && request.weather[0].id <= 800) {
+    $("#app-weather-data-temperature")
+      .querySelector("p")
+      .querySelector("span").innerText = "Skyfrit";
+    $("#app").removeAttribute("class");
+    $("#app").classList.toggle("bg-neutral");
+  }
+
+  if (request.weather[0].id >= 801 && request.weather[0].id <= 804) {
+    $("#app-weather-data-temperature")
+      .querySelector("p")
+      .querySelector("span").innerText = "Overskyet";
+    $("#app").removeAttribute("class");
+    $("#app").classList.toggle("bg-snow");
+  }
 };
 
 // Add event listener for toggling favourites panel
@@ -85,6 +139,10 @@ $("#app-header")
         // Add event listener to list item
         item.addEventListener("click", (e) => {
           e.preventDefault();
+
+          // Update base app layout
+          update({ lat: location.lat, lon: location.lon });
+
           $("#search").classList.toggle("show");
           $("#app-header").classList.toggle("hide-left");
         });
@@ -116,18 +174,20 @@ $("#search")
   });
 
 // Add event listener for clearing searchHistory
-$("#search-history").querySelector("button").addEventListener("click", (e) => {
-  e.preventDefault();
+$("#search-history")
+  .querySelector("button")
+  .addEventListener("click", (e) => {
+    e.preventDefault();
 
-  // Reset to empty array
-  searchHistory.length = 0;
+    // Reset to empty array
+    searchHistory.length = 0;
 
-  // Update localStorage with new searchHistory
-  localStorage.setItem("searchHistory", JSON.stringify(searchHistory));
+    // Update localStorage with new searchHistory
+    localStorage.setItem("searchHistory", JSON.stringify(searchHistory));
 
-  // Hide searchHistory
-  $("#search-history").style.display = "none";
-})
+    // Hide searchHistory
+    $("#search-history").style.display = "none";
+  });
 
 // Handle location searching
 $("#search")
@@ -161,6 +221,9 @@ $("#search")
 
         // Update localStorage with new searchHistory
         localStorage.setItem("searchHistory", JSON.stringify(searchHistory));
+
+        // Update base app layout
+        update({ lat: location.lat, lon: location.lon });
 
         // Reset searchTab
         $("#search").classList.toggle("show");
